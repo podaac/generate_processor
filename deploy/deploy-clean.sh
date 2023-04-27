f55fbf142b75df70186fc230236b22a7d2f07942
Nikki Tebaldi <17799906+nikki-t@users.noreply.github.com>
2023-04-27 22:27:17 +0000

    Select IDL license file based on venue

--- deploy/deploy-clean.sh
+++ deploy/deploy-clean.sh
@@ -2,11 +2,12 @@
 #
 # Script to delete required IDL installer and license file.
 # 
-# Example usage: ./deploy-clean.sh "s3://bucket"
+# Example usage: ./deploy-clean.sh
 
 ROOT_PATH="$PWD"
+ROOT_LIST=$(ls $ROOT_PATH/idl/install/)
 
-rm -rf $ROOT_PATH/idl/install/idl882-linux.tar.gz
-rm -rf $ROOT_PATH/idl/install/lic_server.dat
+rm -rf ${ROOT_PATH}/idl/install/idl882-linux.tar.gz
+rm -rf ${ROOT_PATH}/idl/install/*.dat
 
 echo "Removed IDL installer and license files."
--- deploy/deploy-idl.sh
+++ deploy/deploy-idl.sh
@@ -5,10 +5,21 @@
 # Command line arguments:
 # [1] s3_bucket: URI of S3 bucket to get files from
 # 
-# Example usage: ./deploy-idl.sh "s3://bucket"
+# Example usage: ./deploy-idl.sh "SIT" "s3://bucket"
 
-S3_BUCKET=$1
+VENUE=$1
+S3_BUCKET=$2
 ROOT_PATH="$PWD"
 
+# Determine correct license file to download
+if [ $VENUE = 'SIT' ]; then
+    LICENSE_FILE=sintegration_lic_server.dat
+elif [ $VENUE = 'UAT' ]; then
+    LICENSE_FILE=uacceptance_lic_server.dat
+else
+    LICENSE_FILE=production_lic_server.dat
+fi
+
+# Download files
 aws s3 cp $S3_BUCKET/idl882-linux.tar.gz $ROOT_PATH/idl/install/idl882-linux.tar.gz
-aws s3 cp $S3_BUCKET/sintegration_lic_server.dat $ROOT_PATH/idl/install/lic_server.dat
+aws s3 cp $S3_BUCKET/$LICENSE_FILE $ROOT_PATH/idl/install/lic_server.dat
